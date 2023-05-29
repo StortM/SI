b764c5ec91af3dd2da89daf3c52bfa3f8c86b30b
Michael Fuglø <michael@fuglo.com>
2023-05-29 14:48:47 +0200

    added more thorough data format examples for python

--- 02-data-formats/python/script.py
+++ 02-data-formats/python/script.py
@@ -1,31 +1,35 @@
 import csv
-from xml.dom import minidom
+import xmltodict
 import yaml
 
 # .txt file
 txtDict = {}
-with open('01-file-format-bonanza/test-files/test.txt', 'r') as f:
+
+with open('test-files/test.txt', 'r') as f:
     for line in f:
-        key, value = line.strip().split(" ")
+        key, value = line.strip().split(":")
         txtDict[key] = value
         
 print(txtDict)
  
 # .csv file
-with open('01-file-format-bonanza/test-files/test.csv', mode='r') as infile:
+csvDict = {}
+with open('test-files/test.csv', mode='r') as infile:
     reader = csv.DictReader(infile, skipinitialspace=True)
-    d = [r for r in reader]
-    print(d)
+    csvDict = [r for r in reader]
+    
+print(csvDict)
 
 # .xml file
-dom = minidom.parse('01-file-format-bonanza/test-files/test.xml')
-elements = dom.getElementsByTagName('student')
+with open('test-files/test.xml', 'r', encoding='utf-8') as file:
+    xmlFromFile = file.read()
 
-print('\nAll student data:')
-for elem in elements:
-    print(elem.toxml())
+xmlDict = xmltodict.parse(xmlFromFile)
+print(xmlDict)
 
 # .yml file
-with open('01-file-format-bonanza/test-files/test.yml') as f:
-    my_dict = yaml.safe_load(f)
-    print(my_dict)
+ymlDict = {}
+with open('test-files/test.yml') as f:
+    ymlDict = yaml.safe_load(f)
+
+print(ymlDict)
