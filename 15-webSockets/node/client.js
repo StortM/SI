e2ebca1d4f419b4103760b6131cc343a092fd766
Michael Fuglø <michael@fuglo.com>
2023-06-01 00:26:46 +0200

    added websocket communication with messages and ids

--- 15-webSockets/node/client.js
+++ 15-webSockets/node/client.js
@@ -1,8 +1,29 @@
 import WebSocket from "ws";
+import readline from "readline";
 
-const webSocketClient = new WebSocket("ws://localhost:8080");
+const webSocketClient = new WebSocket("ws://localhost:3000");
 
 webSocketClient.on("open", () => {
   console.log("Connected to server");
-  webSocketClient.send("Hello from client");
+
+  // Create readline interface to read messages from console
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  // Read messages from console and send to server
+  rl.on('line', (input) => {
+    // Send the message to the server
+    webSocketClient.send(input);
+  });
 });
+
+webSocketClient.on("message", (event) => {
+  console.log(`Received message: ${event}`);
+});
+
+webSocketClient.on("close", () => {
+  console.log('WebSocket connection closed');
+});
+
--- 15-webSockets/node/server.js
+++ 15-webSockets/node/server.js
@@ -1,18 +1,31 @@
 import { WebSocketServer } from "ws";
 
-const server = new WebSocketServer({port: 8080});
+
+const PORT = 3000;
+const server = new WebSocketServer({ port: PORT });
+
+console.log("WebSocket server running on port", PORT);
 
 server.on("connection", (ws) => {
   console.log("New connection", server.clients.size);
 
+  // generate a unique identifier for each client
+  const uniqueIdentifier = Math.random().toString(36).substr(2, 9);
+  ws.send("Successfully connected to server. You have been given id: " + uniqueIdentifier);
+
+
   ws.on("message", (message) => {
-    console.log(`Received message: ${message}`);
+    console.log(`Received message: ${message} from client with id: ${uniqueIdentifier}`);
 
     console.log("Client IP:", ws._socket.remoteAddress);
     console.log("Client Unique Identifier:", ws._socket._peername);
 
     server.clients.forEach((client) => {
-      client.send(`A message was sent: ${message}`);
+      client.send(`A message was sent from client with id ${uniqueIdentifier} : ${ message }`);
     });
+  });
+
+  ws.on("close", () => {
+    console.log("Client disconnected with id : ", uniqueIdentifier, "Remaining clients online : ", server.clients.size);
   });
 });
